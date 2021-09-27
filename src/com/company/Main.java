2cadcbf3d9b562b9d62d951834cb08a0c831bf15
Michael Davies <MD210515@reigate.ac.uk>
2021-09-27 10:54:57 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter a minimum number");
+        int min =  input.nextInt();
+        System.out.println("please enter a maximum nuymber");
+        int max = input.nextInt();
+        Random randomNumber = new Random();
+        int count = 0;
+        while(count < 5);
+        System.out.println(randomNumber);
+        count = count + 1;
+	// write your code here
+    }
+}
